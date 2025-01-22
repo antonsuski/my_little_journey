f47330728a8838feb45ebf3ef5019b9ec2f806e0
anton <antonsyski@gmail.com>
2025-01-22 15:38:36 +0300

    added notification logic

--- programming_languages/cxx/labs/patterns/observer/main.cxx
+++ programming_languages/cxx/labs/patterns/observer/main.cxx
@@ -12,5 +12,15 @@ int main (int argc, char** argv)
     alarm.add_observer (&second);
 
     std::cout << "Hello, World!" << std::endl;
+    std::cout << alarm << "\nNotifing alaram follrowers ...\n";
+    alarm.send (event::on_notify);
+
+    std::cout << "Removing followers ...\n";
+    alarm.rem_observer (&first);
+    std::cout << alarm << std::endl;
+    alarm.rem_observer (&second);
+    std::cout << alarm << std::endl;
+
+    alarm.send (event::on_dying);
     return 0;
 }
--- programming_languages/cxx/labs/patterns/observer/obs.hxx
+++ programming_languages/cxx/labs/patterns/observer/obs.hxx
@@ -1,15 +1,40 @@
+#pragma once
+
 #include <algorithm>
 #include <iostream>
 #include <list>
+#include <vector>
+
+const static std::vector<std::string_view> names{ "reserved", "on_notify",
+                                                  "on_dying" };
+enum class event
+{
+    on_notify = 1,
+    on_dying  = 2,
+};
+
+std::ostream& operator<< (std::ostream& out, const event e)
+{
+    out << names[static_cast<size_t> (e)];
+    return out;
+}
 
 class observer
 {
     static int s_counter;
+    int        m_id;
 
 public:
-    virtual void on_notify () { std::cout << s_counter << "\n"; }
-    observer () { s_counter++; }
+    virtual void on_notify (event e)
+    {
+        std::cout << "id:" << m_id << "; is notified: " << e << "\n";
+    }
+    observer ()
+        : m_id{ s_counter++ }
+    {
+    }
     ~observer () { s_counter--; }
+    const int get_id () const { return m_id; }
 };
 
 int observer::s_counter{ 0 };
@@ -24,11 +49,11 @@ public:
     {
     }
 
-    void on_notify ()
+    void on_notify (event e)
     {
         if (nullptr != m_observer)
         {
-            m_observer->on_notify ();
+            m_observer->on_notify (e);
         }
     }
 
@@ -37,12 +62,16 @@ public:
         return this->m_observer == rhs.m_observer;
     }
 
+    const int get_id () const
+    {
+        if (nullptr != m_observer)
+        {
+            return m_observer->get_id ();
+        }
+        return -1;
+    }
+
     friend std::ostream& operator<< (std::ostream& out, const node& lhs);
-    // friend std::ostream& operator<<(std::ostream& out, const node& lhs)
-    // {
-    //     out << lhs.m_observer;
-    //     return out;
-    // }
 };
 
 std::ostream& operator<< (std::ostream& out, const node& lhs)
@@ -53,24 +82,39 @@ std::ostream& operator<< (std::ostream& out, const node& lhs)
 
 class subject
 {
-    std::list<node> followers{};
+    std::list<node> m_followers{};
 
 public:
-    void add_observer (observer* const o) { followers.emplace_back (o); };
+    void add_observer (observer* const o) { m_followers.emplace_back (o); };
+
     void rem_observer (observer* const o)
     {
-        auto it = std::find (followers.begin (), followers.end (), o);
-        if (followers.end () != it)
+        auto it = std::find (m_followers.begin (), m_followers.end (), o);
+        if (m_followers.end () != it)
         {
-            followers.erase (it);
+            m_followers.erase (it);
         }
     }
-    void print_followers ()
+
+    int send (event e)
     {
-        std::cout << "total followers: " << followers.size ();
-        for (auto&& it : followers)
+        for (auto&& it : m_followers)
         {
-            // std::cout << it;
+            it.on_notify (e);
         }
+
+        return 69;
     }
+
+    friend std::ostream& operator<< (std::ostream& out, const subject& lhs);
 };
+
+std::ostream& operator<< (std::ostream& out, const subject& lhs)
+{
+    out << "total followers: " << lhs.m_followers.size () << '\n';
+    for (auto&& it : lhs.m_followers)
+    {
+        out << "follower [" << it.get_id () << "]: " << it << ';';
+    }
+    return out;
+}
